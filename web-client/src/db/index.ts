b461f8168690b9e86b0941a4aa18fcda6b7d53f0
enricobertolotti <hello@enricobertolotti.com>
2021-02-23 00:03:12 +0100

    Add dockerfile and ignore

---
+++ web-client/src/db/index.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+// Connection URI
+const uri = "mongodb://devroot:devroot@localhost:27017/?authSource=admin";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- web-client/src/store/index.ts
+++ web-client/src/store/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import "@/db/index";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
